195e93c7c9e37889b55628cf410ec9bef37d8483
Michael Pingel <mdpingel@gmail.com>
2021-02-05 11:57:09 -0800

    write Calculations.venus test

--- __tests__/Calculations.test.js
+++ __tests__/Calculations.test.js
@@ -10,4 +10,8 @@ describe("Calculations", () => {
   test("should return a float of the difference between Mercury's and Earth's orbit in the form of a ratio", () => {
     expect(calcs.mercury).toEqual(0.24);
   });
+
+  test("should return a float of the difference between Venus' and Earth's orbit in the form of a ration", () => {
+    expect(calcs.venus).toEqual(0.62);
+  })
 }); 
