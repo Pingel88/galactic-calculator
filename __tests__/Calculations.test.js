73ae190cc62513c53b21e46d84ff6d33f566e0ef
Michael Pingel <mdpingel@gmail.com>
2021-02-05 12:01:00 -0800

    write Calculations.mars test and get a good fail because the constructor already exists

--- __tests__/Calculations.test.js
+++ __tests__/Calculations.test.js
@@ -11,7 +11,11 @@ describe("Calculations", () => {
     expect(calcs.mercury).toEqual(0.24);
   });
 
-  test("should return a float of the difference between Venus' and Earth's orbit in the form of a ration", () => {
+  test("should return a float of the difference between Venus' and Earth's orbit in the form of a ratio", () => {
     expect(calcs.venus).toEqual(0.62);
+  })
+
+  test("should return a float of the difference between Mars' and Earth's orbit in the form of a ratio", () => {
+    expect(calcs.mars).toEqual(1.88);
   })
 }); 
