e892bfd7a495edaef6e037c91cf71cf8bc17650b
Michael Pingel <mdpingel@gmail.com>
2021-02-05 14:58:25 -0800

    write User.lifeExpectancy() test

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -3,9 +3,11 @@ import Calculations from "./../src/js/Calculations.js";
 
 describe("User", () => {
   let currentUser;
+  let calc;
 
   beforeEach(() => {
     currentUser = new User("Mike", 32);
+    calc = new Calculations();
   });
 
   test("should assign the 'name' key to a given string", () => {
@@ -19,6 +21,11 @@ describe("User", () => {
   test("should return the global life expectancy when the key is called", () => {
     expect(currentUser.lifeExpectancy).toEqual(72.8);
   });
+
+  test("should update the User.lifeExpectancy value based on the country given", () => {
+    currentUser.updateLifeExpectancy(calc.turkey);
+    expect(currentUser.lifeExpectancy).toEqual(77.7);
+  })
 
   // test("should return user's remaining life expectancy in the given planet's years based on the user's country", () => {
   //   expect(currentUser.yearsLeft())
