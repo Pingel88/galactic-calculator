aa3e89dab9a34fe156729d3b21e07153f49c8c3a
Michael Pingel <mdpingel@gmail.com>
2021-02-05 10:44:44 -0800

    create user.js and user.test.js, write test for empty user class

---
+++ __tests__/User.test.js
@@ -0,0 +1,8 @@
+import { TestScheduler } from "jest";
+import User from "./../src/js/User.js";
+
+describe("User", () => {
+  const currentUser = new User();
+  test("should create a new instance of the User class");
+    expect(currentUser).toEqual(User {});
+});
---
+++ src/js/biz-logic.js
@@ -0,0 +1,257 @@
+user class
+age - user entered
+
+
+
+use life expectancy from the top 50 most populated countries?
+
+
+planets class
+mercury * .24
+venus * .62
+mars * 1.88
+jupiter * 11.86
+
+
+
+
+
+
+
+china
+india
+united states 
+indonesia
+pakistan
+brazil
+nigeria
+bangladesh
+russia
+mexico
+japan
+phillipines
+ethiopa
+democratic republic of congo 
+egypt
+vietnam
+iran 
+turkey
+germany
+france
+united kingdom
+thailand
+south africa
+tanzania
+italy
+myanmar
+south korea
+colombia
+kenya 
+spain 
+argentina
+algeria
+sudan 
+uganda
+ukraine 
+iraq 
+canada 
+poland
+morocco
+uzbekistan
+saudi arabia
+peru 
+afghanistan
+malaysia
+
+
+
+Afghanistan	64.8
+Albania	78.6
+Algeria	76.9
+Andorra	81.9
+Angola	61.2
+Antigua and Barbuda	77
+Argentina	76.7
+Armenia	75.1
+Australia	83.4
+Austria	81.5
+Azerbaijan	73
+Bahamas	73.9
+Bahrain	77.3
+Bangladesh	72.6
+Barbados	79.2
+Belarus	74.8
+Belgium	81.6
+Belize	74.6
+Benin	61.8
+Bhutan	71.7
+Bolivia	71.5
+Bosnia and Herzegovina	77.4
+Botswana	69.6
+Brazil	75.9
+Brunei	75.9
+Bulgaria	75.1
+Burkina Faso	61.6
+Burundi	61.6
+Cabo Verde	73
+Cambodia	69.8
+Cameroon	59.3
+Canada	82.4
+Central African Republic	53.3
+Chad	54.2
+Chile	80.2
+Colombia	77.3
+Comoros	64.3
+Congo, Democratic Republic of the	60.7
+Congo, Republic of	64.6
+Costa Rica	80.3
+Côte d'Ivoire	57.8
+Croatia	78.5
+Cuba	78.8
+Cyprus	81
+Czech Republic	79.4
+Denmark	80.9
+Djibouti	67.1
+Dominica	78.2
+Dominican Republic	74.1
+Ecuador	77
+Egypt	72
+El Salvador	73.3
+Equatorial Guinea	58.7
+Eritrea	66.3
+Estonia	78.8
+Eswatini	60.2
+Ethiopia	66.6
+Federated States of Micronesia	67.9
+Fiji	67.4
+Finland	81.9
+France	82.7
+Gabon	66.5
+Gambia	62.1
+Georgia	73.8
+Germany	81.3
+Ghana	64.1
+Greece	82.2
+Grenada	72.4
+Guatemala	74.3
+Guinea	61.6
+Guinea-Bissau	58.3
+Guyana	69.9
+Haiti	64
+Honduras	75.3
+Hong Kong	84.9
+Hungary	76.9
+Iceland	83
+India	69.7
+Indonesia	71.7
+Iran	76.7
+Iraq	70.6
+Ireland	82.3
+Israel	83
+Italy	83.5
+Jamaica	74.5
+Japan	84.6
+Jordan	74.5
+Kazakhstan	73.5
+Kenya	66.7
+Kiribati	68.4
+Kuwait	75.5
+Kyrgyzstan	71.5
+Laos	67.9
+Latvia	75.3
+Lebanon	78.9
+Lesotho	54.3
+Liberia	64.1
+Libya	72.9
+Liechtenstein	80.7
+Lithuania	75.9
+Luxembourg	82.3
+Madagascar	67
+Malawi	64.3
+Malaysia	76.2
+Maldives	78.9
+Mali	59.3
+Malta	82.5
+Marshall Islands	74.1
+Mauritania	64.9
+Mauritius	75
+Mexico	75.1
+Moldova	71.9
+Mongolia	69.9
+Montenegro	76.9
+Morocco	76.7
+Mozambique	60.9
+Myanmar	67.1
+Namibia	63.7
+Nepal	70.8
+Netherlands	82.3
+New Zealand	82.3
+Nicaragua	74.5
+Niger	62.4
+Nigeria	54.7
+North Macedonia	75.8
+Norway	82.4
+Oman	77.9
+Pakistan	67.3
+Palau	73.9
+Palestine	74.1
+Panama	78.5
+Papua New Guinea	64.5
+Paraguay	74.3
+People's Republic of China	76.9
+Peru	76.7
+Philippines	71.2
+Poland	78.7
+Portugal	82.1
+Qatar	80.2
+Romania	76.1
+Russia	72.6
+Rwanda	69
+Saint Kitts and Nevis	74.8
+Saint Lucia	76.2
+Saint Vincent and the Grenadines	72.5
+Samoa	73.3
+Sao Tome and Principe	70.4
+Saudi Arabia	75.1
+Senegal	67.9
+Serbia	76
+Seychelles	73.4
+Sierra Leone	54.7
+Singapore	83.6
+Slovakia	77.5
+Slovenia	81.3
+Solomon Islands	73
+South Africa	64.1
+South Korea	83
+South Sudan	57.9
+Spain	83.6
+Sri Lanka	77
+Sudan	65.3
+Suriname	71.8
+Sweden	82.8
+Switzerland	83.8
+Syria	72.7
+Tajikistan	71.1
+Tanzania	65.5
+Thailand	77.2
+Timor Leste	69.5
+Togo	61
+Tonga	70.9
+Trinidad and Tobago	73.6
+Tunisia	76.7
+Turkey	77.7
+Turkmenistan	68.2
+Uganda	63.4
+Ukraine	72.1
+United Arab Emirates	78
+United Kingdom	81.3
+United States	78.9
+Uruguay	77.9
+Uzbekistan	71.7
+Vanuatu	70.5
+Venezuela	72.1
+Vietnam	75.4
+World	72.8
+Yemen	66.1
+Zambia	63.9
+Zimbabwe	61.5
