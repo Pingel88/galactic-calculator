2c07a1535e4bb31257d592a12f0cada4a5759c6b
Michael Pingel <mdpingel@gmail.com>
2021-02-05 10:56:04 -0800

    rewrite user test to test the value of the name key

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -2,7 +2,8 @@ import { TestScheduler } from "jest";
 import User from "./../src/js/User.js";
 
 describe("User", () => {
-  const currentUser = new User();
-  test("should create a new instance of the User class");
-    expect(currentUser).toEqual(User {});
+  test("should create a new instance of the User class and assign the 'name' key to a given string", () => {
+  const currentUser = new User("Mike");
+  expect(currentUser.name).toEqual("Mike");
+  })
 });
