2358e0c76d6f724e29f3a4dc696913ef5c78470d
Michael Pingel <mdpingel@gmail.com>
2021-02-06 13:28:44 -0800

    rename javascript files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Calculations from './js/calculations.js';
+import User from './js/user.js';
